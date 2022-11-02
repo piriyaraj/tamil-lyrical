83355414eccf5e17f36977b37253b696bc8c2dfb
piriyaraj <piriyaraj1998@gmail.com>
2022-11-02 08:38:18 +0530

    added superuser access for extract

--- extract/views.py
+++ extract/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import user_passes_test
 from blog.models import Movie
 from . import tools
 # Create your views here.
 def index(request):
     return render(request,'extract/index.html')
 
+
+@user_passes_test(lambda u: u.is_superuser)
 def getNewLyrics(request):
    
     tools.run()
 
     return render(request,'extract/index.html')
 
+
+@user_passes_test(lambda u: u.is_superuser)
 def update(request):
     tools.updateUrl()
 
